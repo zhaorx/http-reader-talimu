41f9e17b90a984a4fbff0f8bb717f04795849aed
zhaorx <zhaorenxiang1988@gmail.com>
2022-12-05 16:53:15 +0800

    talimu done

--- src/main/java/com/hy/http/SchedulerTask.java
+++ src/main/java/com/hy/http/SchedulerTask.java
@@ -3,7 +3,9 @@ package com.hy.http;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import com.hy.http.model.*;
+import com.sun.jndi.toolkit.url.Uri;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,10 +15,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class SchedulerTask {
@@ -29,6 +30,8 @@ public class SchedulerTask {
     private String UserAuthorityCode;
     @Value("${region}")
     private String region;
+    @Value("#{${unit-map}}")
+    private Map<String, String> unitMap;
     @Value("${points:}")
     private String[] points;
     private String sep = "_";
@@ -36,30 +39,31 @@ public class SchedulerTask {
     @Scheduled(fixedDelayString = "${interval}")
     public void transferSchedule() {
         logger.info("starting transfer...");
-        Result r = this.getRecentData();
+        List<DataItem> list = this.getRecentData();
 
         Gas g = new Gas();
         g.setRegion(region);
 
-        if (r.getData() == null) {
+        if (list == null) {
             return;
         }
 
-        for (int i = 0; i < r.getData().size(); i++) {
-            DataItem item = r.getData().get(i);
+        for (int i = 0; i < list.size(); i++) {
+            DataItem item = list.get(i);
 
             g.setTs(item.getTimeStamp());
             g.setPoint(region + sep + item.getTagName());
             g.setPname(region + sep + item.getTagName());
             g.setValue(item.getValue());
             g.setUnit(item.getUnits());
+            g.setUnit(unitMap.get(item.getTagName()));
 
             WritterResult result = this.addTaos(g);
             logger.info(result.getMessage());
         }
     }
 
-    public Result getRecentData() {
+    public List<DataItem> getRecentData() {
         HttpHeaders headers = new HttpHeaders();
         RestTemplate restTemplate = new RestTemplate();
         headers.add("Content-Type", "application/x-www-form-urlencoded");
@@ -67,9 +71,20 @@ public class SchedulerTask {
                 .queryParam("UserAuthorityCode", UserAuthorityCode)
                 .queryParam("tagsStr", points);
         HttpEntity<JSONObject> request = new HttpEntity<>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(builder.build().encode().toUri(), HttpMethod.POST, request, String.class);
-        Result result = JSON.parseObject(response.getBody(), Result.class);
-        return result;
+        URI uri = builder.build().encode().toUri();
+//        logger.info("##########\n" + uri.toString());
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
+        String str = response.getBody();
+        List<DataItem> list = new ArrayList<>();
+//        logger.info("!!!!!!!!!!!!!\n" + str + '\n');
+        if (!"".equals(str) && str.length() > 0) {
+            str = str.replace('\'', '\"');
+            str = str.substring(1, str.length() - 1);
+            list = JSON.parseObject(str, new TypeReference<ArrayList<DataItem>>() {
+            });
+        }
+
+        return list;
     }
 
     private WritterResult addTaos(Gas data) {
